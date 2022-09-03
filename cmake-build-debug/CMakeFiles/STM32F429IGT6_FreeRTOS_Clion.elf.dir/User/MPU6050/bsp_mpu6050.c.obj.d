6908d1f27419e8d3d5fdc0eca5f318bafe0d9b53
uta2012 <2267176649@qq.com>
2022-09-03 11:32:05 +0800

    1

---
+++ cmake-build-debug/CMakeFiles/STM32F429IGT6_FreeRTOS_Clion.elf.dir/User/MPU6050/bsp_mpu6050.c.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/STM32F429IGT6_FreeRTOS_Clion.elf.dir/User/MPU6050/bsp_mpu6050.c.obj: \
+ D:\STM32F429IGT6_FreeRTOS_Clion\User\MPU6050\bsp_mpu6050.c \
+ D:\STM32F429IGT6_FreeRTOS_Clion\User\MPU6050\bsp_mpu6050.h
